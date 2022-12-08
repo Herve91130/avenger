6653ed246d12052dfa0a95f7a75a0d33867598ff
Nans MargezJacmot <margezjacmotnans@gmail.com>
2022-12-08 09:26:13 +0100

    Code de James Buchanan Barnes

--- js/style.js
+++ js/style.js
@@ -1,1 +1,10 @@
-//create new style
+nFilter.style.width = '330px';
+nFilter.style.float = 'left';
+
+<style>
+[title="MyTitle"] {
+    display: none;
+}
+</style>
+<div title="MyTitle">MyTitle</div>
+<div title="SomethingElse">SomethingElse</div>
